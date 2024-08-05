d60b2bf61f22ff16d822e131213b25b46617613f
jlkruger94 <jlkruger94@gmail.com>
2024-08-05 20:43:52 -0300

    Ajustes sobre la biblioteca del RTC

--- Core/drivers/RTC/src/RTC_DS3231_hw.c
+++ Core/drivers/RTC/src/RTC_DS3231_hw.c
@@ -5,11 +5,12 @@
  *      Author: José Luis Krüger
  */
 #include "RTC_DS3231_hw.h"
+#define MAX_DELAY (MAX_TRANSMISSION_TIMEOUT * COMM_RETRIES)
 
 static I2C_HandleTypeDef *i2c_handler;
-static const uint32_t MAX_TRANSMISSION_TIMEOUT = 15000;
+static const uint32_t MAX_TRANSMISSION_TIMEOUT = 1500;
+static const uint32_t COMM_RETRIES = 5;
 
-static void RTC_DS3231_hw_delay(uint16_t delay);
 static HAL_StatusTypeDef RTC_DS3231_hw_I2C_transmit(uint8_t *data, uint16_t size);
 static HAL_StatusTypeDef RTC_DS3231_hw_I2C_receive(uint8_t *data, uint16_t size);
 
@@ -48,31 +49,22 @@ bool_t RTC_DS3231_hw_set_I2C_handler(void *handler) {
 
 static HAL_StatusTypeDef RTC_DS3231_hw_I2C_transmit(uint8_t *data, uint16_t size) {
     HAL_StatusTypeDef ret;
+    delay_t timeout;
+    DelayInit(&timeout,MAX_DELAY);
     do {
         ret = HAL_I2C_Master_Transmit(i2c_handler, RTC_DS3231_ADDRESS << 1, data, size, MAX_TRANSMISSION_TIMEOUT);
-    } while (ret == HAL_BUSY);
+    } while (ret == HAL_BUSY && !DelayRead(&timeout));
 
     return ret;
 }
 
 static HAL_StatusTypeDef RTC_DS3231_hw_I2C_receive(uint8_t *data, uint16_t size) {
     HAL_StatusTypeDef ret;
+    delay_t timeout;
+    DelayInit(&timeout,MAX_DELAY);
     do {
         ret = HAL_I2C_Master_Receive(i2c_handler, RTC_DS3231_ADDRESS << 1, data, size, MAX_TRANSMISSION_TIMEOUT);
-    } while (ret == HAL_BUSY);
+    } while (ret == HAL_BUSY && !DelayRead(&timeout));
 
     return ret;
-}
-
-static void RTC_DS3231_hw_delay(uint16_t delay) {
-    if (delay == 0) return;
-
-    static delay_t delay_struc = {
-        .duration = 0,
-        .running = false,
-        .startTime = 0
-    };
-
-    DelayWrite(&delay_struc, delay);
-    while (!DelayRead(&delay_struc));
 }
